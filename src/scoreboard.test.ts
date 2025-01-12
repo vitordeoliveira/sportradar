e6c758ac0effed486f2e61fbe2a351bea3af4403
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2025-01-12 11:32:37 +0000

    add retrive match in a describe block

--- src/scoreboard.test.ts
+++ src/scoreboard.test.ts
@@ -3,6 +3,9 @@ describe("Scoreboard", () => {
   it("Should Scoreboard new match initiate with 0 - 0 receiving home and away team as parameters", () => {});
   it("Should update score receive a pair of absolute scores", () => {});
   it("Should finish the match remove match from scoreboard", () => {});
-  it("Should retrieve all current in progress matches ordered by total score", () => {});
-  it("Should retrieve all current in progress matches ordered by total score, when total score is the same it should return last added match before", () => {});
+
+  describe("Scoreboard retrieve matches", () => {
+    it("Should retrieve ordered by total score", () => {});
+    it("Should ordered by the most recently started match when total score is the same", () => {});
+  });
 });
