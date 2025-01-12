90a06a3fc527fc30ebab9c9fb9678bf521d40698
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2025-01-12 15:53:05 +0000

    add extra tests to F1

--- src/scoreboard.test.ts
+++ src/scoreboard.test.ts
@@ -12,24 +12,60 @@ describe("Scoreboard", () => {
     const scoreboard = new Scoreboard(db);
     expect(scoreboard).toBeDefined();
   });
-  it("Should Scoreboard new match initiate with 0 - 0 receiving home and away team as parameters", () => {
-    const db: InMemoryDatabase = {};
-    const scoreboard = new Scoreboard(db);
 
-    let game: Match = scoreboard.addMatch("Team home", "Team away");
+  describe("create match", () => {
+    it("Should Scoreboard new match initiate with 0 - 0 receiving home and away team as parameters", () => {
+      const db: InMemoryDatabase = {};
+      const scoreboard = new Scoreboard(db);
 
-    const id = `Team home-Team away-0`;
-    let match = {
-      id,
-      homeTeam: "Team home",
-      awayTeam: "Team away",
-      homeScore: 0,
-      awayScore: 0,
-      finished: false,
-    };
-    expect(game).toEqual(match);
+      let game: Match = scoreboard.addMatch("Team home", "Team away");
+
+      const id = `Team home-Team away-0`;
+      let match = {
+        id,
+        homeTeam: "Team home",
+        awayTeam: "Team away",
+        homeScore: 0,
+        awayScore: 0,
+        finished: false,
+      };
+      expect(game).toEqual(match);
+      expect(db[match.id]).toEqual(match);
+    });
+
+    it("should create multiple matches", () => {
+      const db: InMemoryDatabase = {};
+      const scoreboard = new Scoreboard(db);
+
+      let game1: Match = scoreboard.addMatch("Team home", "Team away");
+      let game2: Match = scoreboard.addMatch("Team home1", "Team away1");
+
+      const gameId1 = `Team home-Team away-0`;
+
+      let match1 = {
+        id: gameId1,
+        homeTeam: "Team home",
+        awayTeam: "Team away",
+        homeScore: 0,
+        awayScore: 0,
+        finished: false,
+      };
+
+      const gameId2 = `Team home1-Team away1-0`;
+      let match2 = {
+        id: gameId2,
+        homeTeam: "Team home1",
+        awayTeam: "Team away1",
+        homeScore: 0,
+        awayScore: 0,
+        finished: false,
+      };
+      expect(game1).toEqual(match1);
+      expect(game2).toEqual(match2);
+      expect(db[gameId1]).toEqual(match1);
+      expect(db[gameId2]).toEqual(match2);
+    });
   });
-
   describe("update match", () => {
     it("Should update score receive a pair of absolute scores", () => {
       const matchId = `Team home-Team away-0`;
