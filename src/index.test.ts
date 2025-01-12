3844912cb42b036d661d81cfc74b2db84a94f64f
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2025-01-12 19:25:52 +0000

    simple impl of the lib

--- src/index.test.ts
+++
@@ -1,5 +0,0 @@
-import { sayHello } from "./index";
-
-test("sayHello returns the correct greeting", () => {
-  expect(sayHello()).toBe("Hello, World!");
-});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,49 @@
-export function sayHello(): string {
-  return "Hello, World!";
-}
+import { Scoreboard } from "./scoreboard";
+import { InMemoryDatabase } from "./database";
 
-console.log(sayHello());
+const db: InMemoryDatabase = {};
+const scoreboard = new Scoreboard(db);
+
+let match1 = scoreboard.addMatch("Brazil", "Germany");
+let match2 = scoreboard.addMatch("France", "Uruguay");
+
+console.log(
+  "------------------------\n  summary\n------------------------\n" +
+    scoreboard.summary() +
+    "\n------------------------\n",
+);
+
+scoreboard.updateMatch(match1.id, { home: 0, away: 1 });
+scoreboard.updateMatch(match2.id, { home: 0, away: 0 });
+
+console.log(
+  "------------------------\n  summary\n------------------------\n" +
+    scoreboard.summary() +
+    "\n------------------------\n",
+);
+
+scoreboard.updateMatch(match1.id, { home: 0, away: 1 });
+scoreboard.updateMatch(match2.id, { home: 1, away: 0 });
+
+console.log(
+  "------------------------\n  summary\n------------------------\n" +
+    scoreboard.summary() +
+    "\n------------------------\n",
+);
+
+let match3 = scoreboard.addMatch("Argentina", "Mexico");
+
+scoreboard.updateMatch(match3.id, { home: 2, away: 0 });
+console.log(
+  "------------------------\n  summary\n------------------------\n" +
+    scoreboard.summary() +
+    "\n------------------------\n",
+);
+
+scoreboard.finishMatch(match2.id);
+
+console.log(
+  "------------------------\n  summary\n------------------------\n" +
+    scoreboard.summary() +
+    "\n------------------------\n",
+);
