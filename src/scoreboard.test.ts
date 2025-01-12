3a11db8bdf088818fca1ee43df70d96a3380940b
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2025-01-12 11:29:08 +0000

    adding requirements as tests

---
+++ src/scoreboard.test.ts
@@ -0,0 +1,8 @@
+describe("Scoreboard", () => {
+  it("Should create Scoreboard class with injected database", () => {});
+  it("Should Scoreboard new match initiate with 0 - 0 receiving home and away team as parameters", () => {});
+  it("Should update score receive a pair of absolute scores", () => {});
+  it("Should finish the match remove match from scoreboard", () => {});
+  it("Should retrieve all current in progress matches ordered by total score", () => {});
+  it("Should retrieve all current in progress matches ordered by total score, when total score is the same it should return last added match before", () => {});
+});
