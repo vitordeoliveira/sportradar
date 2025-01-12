1c83950d3cd9e91d4342f08b3da4b75c88003fca
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2025-01-12 12:19:07 +0000

    F1: new match initial test
    starting F1 (functional requesit)
    
    check on readme to see the indexes

--- src/scoreboard.test.ts
+++ src/scoreboard.test.ts
@@ -1,4 +1,4 @@
-import { InMemoryDatabase } from "./database";
+import { InMemoryDatabase, Match } from "./database";
 import { Scoreboard } from "./scoreboard";
 
 describe("Scoreboard", () => {
@@ -7,7 +7,15 @@ describe("Scoreboard", () => {
     const scoreboard = new Scoreboard(db);
     expect(scoreboard).toBeDefined();
   });
-  it("Should Scoreboard new match initiate with 0 - 0 receiving home and away team as parameters", () => {});
+  it("Should Scoreboard new match initiate with 0 - 0 receiving home and away team as parameters", () => {
+    const db: InMemoryDatabase = {};
+    const scoreboard = new Scoreboard(db);
+
+    let game: Match = scoreboard.addMatch("Team home", "Team away");
+
+    expect(game.awayTeam).toBe(0);
+    expect(game.homeTeam).toBe(0);
+  });
   it("Should update score receive a pair of absolute scores", () => {});
   it("Should finish the match remove match from scoreboard", () => {});
 
